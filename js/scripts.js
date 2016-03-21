15b496347bc3ddd526eafc35c4cddd321af96f58
Connor Cummings and Kyle Wolfson <connor+kyle@>
2016-03-21 09:40:37 -0700

    Add several new calcualtions

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+/*This is our BMI calulator*/
+var bmi = function(height, weight){
+  return weight / height;
+};
+
+/*var height = parseInt(prompt("What is your height in inches?"));
+var weight = parseInt(prompt("What is your wight in pounds?"));
+var resultBmi = weight / height;
+alert(resultBmi);
+*/
+
+
+/*
+var temp = function(fahrenheit){
+  var celsius = parseInt(prompt("What is the temperature in celsius"));
+  fahrenheit = (9/5) * celsius + 32;
+  alert(fahrenheit);
+
+};
+temp(85);
+*/
+
+
+var gallonToLiter = function(gallons){
+  liters = gallons * 3.78541;
+  alert(liters);
+};
+gallonToLiter(25);
